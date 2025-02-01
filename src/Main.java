b61a94d365f2ed0c0fb67889b7682d2e81cb1cd6
SwagKing24 <bittomrc@gmail.com>
2025-02-01 11:08:15 +0100

    miao message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.SQLOutput;
 
+public class Main {
+    public static void main(String[] args){
+        System.out.println("miao");
     }
 }

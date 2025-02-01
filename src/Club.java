3e7ef65f88d236758f046d70c910a75751b4d7d5
SwagKing24 <bittomrc@gmail.com>
2025-02-01 11:58:02 +0100

    inzio scrittura codice

---
+++ src/Club.java
@@ -0,0 +1,20 @@
+public class Club {
+    private String name;
+    private int id;
+    private int budget;
+    private int riskMarketFactor;
+    private int ovrGK;
+    private int ovrDEF;
+    private int ovrMF;
+    private int ovrST;
+
+    public Club(String name, int id, int riskMarketFactor){
+        this.name = name;
+        this.id = id;
+        this.riskMarketFactor = riskMarketFactor;
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("miao");
+        String squadName;
+        String aux;
+        int riskMarketFactor;
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Club> campionato = new ArrayList<>();
+
+        for(int i=0; i<20; i++){
+            System.out.println("inserisci il nome del club");
+            squadName = sc.nextLine();
+            System.out.println("inserisci il risk di mercato della squadra (tra 20 e 80 (%))");
+            aux = sc.nextLine();
+            riskMarketFactor = Integer.parseInt(aux);
+            campionato.add(new Club(squadName, i, riskMarketFactor));
+        }
     }
 }
